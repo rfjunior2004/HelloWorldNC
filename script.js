c899ca32a2910144a99c7832e600a773e1396143
Reinaldo Ferreira Junior <ferreira.junior.reinaldo@escola.pr.gov.br>
2025-07-01 21:12:17 -0300

    a

---
+++ script.js
@@ -0,0 +1,8 @@
+function inserirNome()  {
+    let elemento = document.querySelector("#nome-usuario");
+    elemento
+}
+let linguagens = ["JavaScript","Python","C"];​
+console.log(linguagens[0]) // “JavaScript”
+console.log(linguagens[1]) // “Python”
+console.log(linguagens[2]) // “C”
